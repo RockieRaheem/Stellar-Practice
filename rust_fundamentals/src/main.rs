ffb67cb5e795df81e352b59c4d14bbae5190c259
Kamwanga Raheem <kamwangaraheem2050@gmail.com>
2026-06-23 15:34:59 +0300

    Create a vector to hold the tech stack

--- rust_fundamentals/src/main.rs
+++ rust_fundamentals/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // Create a vector to hold the tech stack
+    let mut tech_stack: Vec<&str> = Vec::new();
+
+    tech_stack.push("Rust");
+    tech_stack.push("JavaScript");
+    tech_stack.push("Python");  
+
+    println!("Tech Stack: {:?}", tech_stack);
+
+    for stack in tech_stack {
+      println!("The stack is: {}", stack);
+    }
 }
